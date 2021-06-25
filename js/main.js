df52f64fc0e875d4f35f8e31720415646e0a4191
nguyen huu nhon <huunhon099@gmail.com>
2021-06-26 01:59:20 +0700

    validate

--- js/main.js
+++ js/main.js
@@ -2,8 +2,11 @@
 var priceGrab, kilometers, WaitTimes, grabType;
 var totalMoney;
 //get element function
+var getEles = function(selector) {
+    return document.querySelectorAll(selector);
+}
 var getEle = function(selector) {
-        return document.querySelectorAll(selector);
+        return document.querySelector(selector);
     }
     //class object PriceGrab
 function PriceGrab(_grabType, _priceFirst, _priceNormal, _priceFar, _priceWaitTime) {
@@ -41,63 +44,96 @@ function PriceGrab(_grabType, _priceFirst, _priceNormal, _priceFar, _priceWaitTi
         return result;
     }
 }
-
-
 //CalculationGrab function event click
 var calculationGrab = function() {
-        grabType = getEle('input[name="selector"]:checked')[0].value;
-        kilometers = parseFloat(getEle('#kilometers')[0].value);
-        WaitTimes = parseFloat(getEle('#WaitTimes')[0].value);
-        switch (grabType) {
-            case 'car':
-                priceGrab = new PriceGrab('car', 8000, 7500, 7000, 2000);
-                break;
-            case 'suv':
-                priceGrab = new PriceGrab('suv', 9000, 8500, 8000, 3000);
-                break;
-            case 'black':
-                priceGrab = new PriceGrab('black', 10000, 8500, 9000, 3500);
-                break;
+
+        kilometers = parseFloat(getEle('#kilometers').value);
+        WaitTimes = parseFloat(getEle('#WaitTimes').value);
+        if (validate(kilometers, WaitTimes) == true) {
+            grabType = getEle('input[name="selector"]:checked').value;
+
+            switch (grabType) {
+                case 'car':
+                    priceGrab = new PriceGrab('car', 8000, 7500, 7000, 2000);
+                    break;
+                case 'suv':
+                    priceGrab = new PriceGrab('suv', 9000, 8500, 8000, 3000);
+                    break;
+                case 'black':
+                    priceGrab = new PriceGrab('black', 10000, 8500, 9000, 3500);
+                    break;
+            }
+            totalMoney = priceGrab.totalPrice(kilometers, WaitTimes);
+            getEle('#xuatTien').innerHTML = totalMoney;
+            getEle('#divThanhTien').style.display = "block";
+            getEle('.errInput').style.display = "none";
+        } else {
+            getEle('#divThanhTien').style.display = "none";
+            getEle('.errInput').style.display = "block";
         }
-        totalMoney = priceGrab.totalPrice(kilometers, WaitTimes);
-        getEle('#xuatTien')[0].innerHTML = totalMoney;
-        getEle('#divThanhTien')[0].style.display = "block";
 
     }
     //function set innerHTML ValueInvoice
-function setValueInvoice(use, price, intoMoney, object) {
-    getEle('[data-object="' + object + '"] td:nth-of-type(2)')[0].innerHTML = use;
-    getEle('[data-object="' + object + '"] td:nth-of-type(3)')[0].innerHTML = price;
-    getEle('[data-object="' + object + '"] td:nth-of-type(4)')[0].innerHTML = intoMoney;
-}
-//invoice
+var setValueInvoice = function(use, price, intoMoney, object) {
+        getEle('[data-object="' + object + '"] td:nth-of-type(2)').innerHTML = use;
+        getEle('[data-object="' + object + '"] td:nth-of-type(3)').innerHTML = price;
+        getEle('[data-object="' + object + '"] td:nth-of-type(4)').innerHTML = intoMoney;
+    }
+    //invoice
 var invoidce = function() {
-    calculationGrab();
-    var itemInvoices = getEle('.invoice__item');
-    var use, price, intoMoney;
-    var object;
-    itemInvoices.forEach(function(e) {
-        object = e.getAttribute('data-object');
-        switch (object) {
-            case 'first-kilometer':
-                use = kilometers < 1 ? kilometers : 1;
-                price = priceGrab.priceFirst;
-                intoMoney = price * use;
-                break;
-            case 'distance':
-                use = kilometers;
-                price = priceGrab.priceCurrent(kilometers);
-                intoMoney = price * use;
-                break;
-            case 'waitTimes':
-                use = WaitTimes;
-                price = WaitTimes > 3 ? priceGrab.priceWaitTime : 0;
-                intoMoney = price;
-                break;
-        }
-        setValueInvoice(use, price, intoMoney, object);
-    });
-    getEle('.totalMoney')[0].innerHTML = totalMoney;
+        calculationGrab();
+        var itemInvoices = getEles('.invoice__item');
+        var use, price, intoMoney;
+        var object;
+        itemInvoices.forEach(function(e) {
+            object = e.getAttribute('data-object');
+            switch (object) {
+                case 'first-kilometer':
+                    use = kilometers < 1 ? kilometers : 1;
+                    price = priceGrab.priceFirst;
+                    intoMoney = price * use;
+                    break;
+                case 'distance':
+                    use = kilometers;
+                    price = priceGrab.priceCurrent(kilometers);
+                    intoMoney = price * use;
+                    break;
+                case 'waitTimes':
+                    use = WaitTimes;
+                    price = WaitTimes > 3 ? priceGrab.priceWaitTime : 0;
+                    intoMoney = price;
+                    break;
+            }
+            setValueInvoice(use, price, intoMoney, object);
+        });
+        getEle('.totalMoney').innerHTML = totalMoney;
 
 
+        document.getElementById("invoice").classList.add("show");
+    }
+    //function validate value input
+var validate = function(kilometers, WaitTimes) {
+    if (kilometers < 0 || isNaN(kilometers) || kilometers == 0 ||
+        WaitTimes < 0 || isNaN(WaitTimes) || WaitTimes == 0 ||
+        getEle('input[name="selector"]:checked') == null) {
+        return false;
+    }
+    return true;
+}
+var checkNegative = function(element) {
+    var parent;
+    if (element.id == "kilometers") {
+        parent = '.kilometers-form';
+    } else {
+        parent = '.WaitTimes-form';
+    }
+    var err = getEle(parent + ' .errorNumber');
+    var line = getEle(parent + ' .focus-input100 .line');
+    if (element.value < 0) {
+        err.style.display = "block";
+        line.style.background = "red";
+    } else {
+        err.style.display = "none";
+        line.style.background = "linear-gradient(45deg, #884747, #e0c04d)";
+    }
 }
